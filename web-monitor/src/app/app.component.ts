a53370864c8cebea2cff35d2685ec34690601ad3
Azrielx86 <edgar1569@outlook.com>
2024-05-08 14:36:00 -0600

    Added icons

--- web-monitor/src/app/app.component.ts
+++ web-monitor/src/app/app.component.ts
@@ -1,12 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {initFlowbite} from "flowbite";
+import * as feather from 'feather-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+  ngAfterViewInit(): void {
+    feather.replace();
+  }
+
   ngOnInit(): void {
     initFlowbite();
   }
